d1f34c67d0896ea3740d413086b03ccdaed0d735
Taras <tarasbatyuchok@gmail.com>
2022-04-20 23:11:07 +0300

    HW-3

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,9 +2,9 @@ import s from "./Header.module.css";
 
 const Header = ({ title, descr})=>{
     return (
-      <header class={s.root}>
-        <div class={s.forest}></div>
-        <div class={s.container}>
+      <header className={s.root}>
+        <div className={s.forest}></div>
+        <div className={s.container}>
           <h1>{title}</h1>
           <p>{descr}</p>
         </div>
